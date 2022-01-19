81e9f3d7e1cf7eed5e6ae0a09a72606f584aa439
Caio <midia@investbankcompany.com.br>
2022-01-19 15:08:42 -0300

    Commit

---
+++ fale-conosco/index.php
@@ -0,0 +1,106 @@
+<?php
+
+    // Page Title
+    $pageTitle = "Fale Conosco";
+
+    // Functions for the operation of the site
+    require_once "../inc/functions.php";
+
+	// HTML Start
+	echo "<!DOCTYPE html>";
+
+	echo "\n<html lang=".'"pt-br"'.">\n";
+
+    // Head Tags
+    require_once "../template-parts/head.php";
+
+    // Body
+    echo "\n    <body>";
+
+    // Preloader
+    require_once "../template-parts/preloader.php";
+
+    // Navbar
+    require_once "../template-parts/navbar.php";
+
+?>
+
+        <section class="breadcrumb_area">
+            <img class="breadcrumb_shap" src="img/breadcrumb/banner_bg.png" alt="">
+            <div class="container">
+                <div class="breadcrumb_content text-center">
+                    <h1 class="f_p f_700 f_size_50 w_color l_height50 mb_20">Fale Conosco</h1>
+                    <p class="f_400 w_color f_size_16 l_height26">Why I say old chap that is spiffing off his nut arse pear shaped plastered<br> Jeffrey bodge barney some dodgy.!!</p>
+                </div>
+            </div>
+        </section>
+
+        <section class="contact_info_area sec_pad bg_color">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-4 pr-0">
+                        <div class="contact_info_item">
+                            <h6 class="f_p f_size_20 t_color3 f_500 mb_20">Endereço do escritório</h6>
+                            <p class="f_400 f_size_15">Av. Brig. Faria Lima, 1451, Guarulhos - SP</p>
+                        </div>
+                        <div class="contact_info_item">
+                            <h6 class="f_p f_size_20 t_color3 f_500 mb_20">Informação de contato</h6>
+                            <p class="f_400 f_size_15"><span class="f_400 t_color3">Telefone:</span> <a href="tel:11986904543">(11) 9 8690-4543</a></p>
+                            <p class="f_400 f_size_15"><span class="f_400 t_color3">Email:</span> <a href="mailto:contato@blessedcorporation.com.br">contato@blessedcorporation.com.br</a></p>
+                        </div>
+                    </div>
+                    <div class="col-lg-8">
+                        <div class="mapbox">
+                            <div id="mapBox" class="row m0" data-lat="-23.44278674" data-lon="-46.52222657" data-zoom="10" data-info="Av. Brig. Faria Lima, 1451, Guarulhos - SP" data-marker="img/map.png" data-mlat="-23.44278674" data-mlon="-46.52222657">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="contact_form">
+                    <h2 class="f_p f_size_22 t_color3 f_600 l_height28 mt_100 mb_40">Deixe uma mensagem</h2>
+                    <form action="contact_process.php" class="contact_form_box" method="post" id="contactForm" novalidate="novalidate">
+                        <div class="row">
+                            <div class="col-lg-6">
+                                <div class="form-group text_box">
+                                    <input type="text" id="name" name="name" placeholder="Seu nome">
+                                </div>
+                            </div>
+                            <div class="col-lg-6">
+                                <div class="form-group text_box">
+                                    <input type="text" name="email" id="email" placeholder="Seu email">
+                                </div>
+                            </div>
+                            <div class="col-lg-12">
+                                <div class="form-group text_box">
+                                    <input type="text" id="subject" name="subject" placeholder="Assunto">
+                                </div>
+                            </div>
+                            <div class="col-lg-12">
+                                <div class="form-group text_box">
+                                    <textarea name="message" id="message" cols="30" rows="10" placeholder="Digite sua mensagem . . ."></textarea>
+                                </div>
+                            </div>
+                        </div>
+                        <button type="submit" class="btn_three">Enviar Mensagem</button>
+                    </form>
+                    <div id="success">Sua mensagem foi enviada com sucesso!</div>
+                    <div id="error">Oops! Há algo errado. Por favor, tente novamente</div>
+                </div>
+            </div>
+        </section>
+
+<?php
+
+    // Footer
+    require_once "../template-parts/footer.php";
+
+    // Necessary functions for the functioning of the site
+    require_once "../vendors/compiled-scripts/scripts.php";
+
+    // Close Body
+    echo "\n    </body>";
+
+    // HTML End
+    echo "\n\n</html>";
+    
+?>
--- inc/functions.php
+++ inc/functions.php
@@ -1,22 +1,230 @@
 <?php
 
-    // Página Atual
-    function paginaAtual($condA) {
-        $a = explode("/", $condA);
-        $b = explode(".", $a[count($a)-1]);
-        $c = $b[count($b)-2];
+    // Begin Session
+    session_start();
 
-        return $c;
+    // Setting Brasilia Timezone
+    function timezone($input) {
+        setlocale(LC_TIME, 'pt_BR', 'pt_BR.utf-8', 'portuguese');
+        date_default_timezone_set('America/Sao_Paulo');
+
+        if ($input == "data") {
+            $output = utf8_encode(strftime(ucwords(strftime('%A')).', '.strftime('%d').' de '.ucwords(strftime('%B')).' de '.strftime('%Y')));
+        } else {
+            $output = date('H:i:s');
+        }
+
+        return $output;
     }
-    
-	// Função SE for IGUAL
-	function iiF($condA, $condB, $retA, $retB) {
-		if (isset($condA) && $condA == $condB) {
+
+    // Function that returns a good morning, afternoon or night greeting
+    function salutation() {
+        $hour = date("H");
+
+        if ($hour >= 12 && $hour < 18) {
+            $reply = "Boa tarde!";
+        } else if ($hour >= 0 && $hour < 12) {
+            $reply = "Bom dia!";
+        } else {
+            $reply = "Boa noite!";
+        }
+        return $reply;
+    }
+
+    // Function that removes accented characters and exchanges spaces for bars
+    function clearText($text){
+        $forbiddenCharacter = Array(",",".","'","\"","&","|","!","#","$","¨","*","(",")","`","´","<",">",";","=","+","§","{","}","[","]","^","~","?","%");
+        $forbiddenLetter = Array('Á','È','ô','Ç','á','è','Ò','ç','Â','Ë','ò','â','ë','Ø','Ñ','À','Ð','ø','ñ','à','ð','Õ','Å','õ','Ý','å','Í','Ö','ý','Ã','í','ö','ã','Î','Ä','î','Ú','ä','Ì','ú','Æ','ì','Û','æ','Ï','û','ï','Ù','®','É','ù','©','é','Ó','Ü','Þ','Ê','ó','ü','þ','ê','Ô','ß','‘','’','‚','“','”','„');
+        $clearLetter = Array('a','e','o','c','a','e','o','c','a','e','o','a','e','o','n','a','d','o','n','a','o','o','a','o','y','a','i','o','y','a','i','o','a','i','a','i','u','a','i','u','a','i','u','a','i','u','i','u','','e','u','c','e','o','u','p','e','o','u','b','e','o','b','','','','','','');
+        $space = Array(' ', '  ', '   ', '    ');
+        $newId = str_replace($forbiddenLetter, $clearLetter, $text);
+        $newId = str_replace($forbiddenCharacter, "", trim($newId));
+        $newId = str_replace($space, '-', trim($newId));
+        return strtolower($newId);
+    }
+
+    // Function that takes the current page that the user is browsing
+    function currentPage() {
+        $afterBar = explode("/", $_SERVER["SCRIPT_NAME"]);
+        $beforeDot = explode(".", $afterBar[count($afterBar)-1]);
+        $currentPage = $beforeDot[count($beforeDot)-2];
+
+        return $currentPage;
+    }
+
+    // Function that removes characters that are not numbers
+    function justNum($str){ 
+        return preg_replace("/[^0-9]/", "", $str); 
+    }
+
+    // Get tracking parameters in the URL
+    function utm($input) {
+
+        // UTM SOURCE
+        if(isset($_GET['utm_source'])) { // Exist utm_source?
+            $utm_source = $_GET['utm_source'];
+            $utm_source_link = "?utm_source=$utm_source";
+        } else { // If utm_source does not exist, $utm_source will have another value
+            $utm_source_link = "?utm_source=Orgânico";
+        }
+
+        // UTM CAMPAIGN
+        if(isset($_GET['utm_campaign'])) { // Exist utm_campaign?
+            $utm_campaign = $_GET['utm_campaign'];
+            $utm_campaign_link = "&utm_campaign=$utm_campaign";
+        }
+
+        // UTM MEDIUM
+        if(isset($_GET['utm_medium'])) { // Exist utm_medium?
+            $utm_medium = $_GET['utm_medium'];
+            $utm_medium_link = "&utm_medium=$utm_medium";
+        }
+
+        // UTM NAME
+        if(isset($_GET['utm_name'])) { // Exist utm_name?
+            $utm_name = $_GET['utm_name'];
+            $utm_name_link = "&utm_name=$utm_name";
+        }
+
+        // UTM TERM
+        if(isset($_GET['utm_term'])) { // Exist utm_term?
+            $utm_term = $_GET['utm_term'];
+            $utm_term_link = "&utm_term=$utm_term";
+        }
+
+        // UTM CONTENT
+        if(isset($_GET['utm_content'])) { // Exist utm_content?
+            $utm_content = $_GET['utm_content'];
+            $utm_content_link = "&utm_content=$utm_content";
+        }
+
+        // Link with all UTM
+        $utm_link = $utm_source_link.$utm_campaign_link.$utm_medium_link.$utm_name_link.$utm_term_link.$utm_content_link;
+
+        if(isset($_REQUEST[$input])) {
+            $output = ucfirst($_GET[$input]);
+        } elseif ($input == "link") {
+            $output = $utm_link;
+        } else {
+            $output = null;
+        }
+
+        return $output;
+    }
+
+    // Client data for autocomplete at Contratar
+    function clientData($input){
+
+        // Nome
+        if(isset($_GET['nome_cliente'])) { // Exist nome_cliente?
+            $nome_cliente = ucfirst($_GET['nome_cliente']);
+            $nome_cliente_link = "&nome_cliente=$nome_cliente";
+        }
+
+        // Email
+        if(isset($_GET['email_cliente'])) { // Exist email_cliente?
+            $email_cliente = $_GET['email_cliente'];
+            $email_cliente_link = "&email_cliente=$email_cliente";
+        }
+            
+        // Telefone
+        if(isset($_GET['telefone_cliente'])) { // Exist telefone_cliente?
+            $telefone_cliente = $_GET['telefone_cliente'];
+            $telefone_cliente_link = "&telefone_cliente=$telefone_cliente";
+        }
+            
+        // Valor
+        if(isset($_GET['valor_cliente'])) { // Exist valor_cliente?
+            $valor_cliente = $_GET['valor_cliente'];
+            $valor_cliente_link = "&valor_cliente=$valor_cliente";
+        } else {
+            $valor_cliente_link = "&valor_cliente=0";
+        }
+
+        // Link with all data
+        $link_client = $nome_cliente_link.$email_cliente_link.$telefone_cliente_link.$valor_cliente_link;
+
+        if(isset($_REQUEST[$input])) {
+            $output = ucfirst($_GET[$input]);
+        } elseif ($input == "link") {
+            $output = $link_client;
+        } else {
+            $output = null;
+        }
+
+        return $output;
+    }
+
+    // Function IF is EQUAL
+    function iiF($condA, $condB, $retA, $retB) {
+        if (isset($condA) && $condA == $condB) {
             return $retA;
-		} else {
+        } else {
             return $retB;
-		}
+        }
     }
-    
-    
-    // $paginaAtual = paginaAtual($_SERVER["SCRIPT_NAME"]);
+
+    // Function IF is DIFFERENT
+    function diiF($condA, $condB, $retA, $retB) {
+        if (isset($condA) && $condA != $condB) {
+            return $retA;
+        } else {
+            return $retB;
+        }
+    }
+
+    // Check the user's device
+    function isMobile($resA, $resB) {
+        $mobile = false;
+        $user_agents = array(
+            "iPhone",
+            "iPad",
+            "Android",
+            "webOS",
+            "BlackBerry",
+            "iPod",
+            "Symbian",
+            "IsGeneric"
+        );
+
+        foreach ($user_agents as $user_agent) {
+            if (strpos($_SERVER['HTTP_USER_AGENT'], $user_agent) !== false) {
+                $mobile = true;
+                break;
+            }
+        }
+
+        if ($mobile) {
+            return $resA;
+        } else {
+            return $resB;
+        }
+    }
+
+    // List images for the Owl Carousel
+    function listImage($folder,$car) {
+        $directory = "img/$folder/$car/";
+        $imgs = glob($directory."*.{jpg,png,gif,webp}", GLOB_BRACE);
+
+        foreach($imgs as $img){
+            echo "
+                <div class='item'>
+                    <img src='$img'>
+                </div>
+            ";
+        }
+    }
+
+    // Connection to the MySql Database
+    $conn = mysqli_connect("mysql.consorciodiretoauto.com.br", "consorciodiret", "investDB1", "consorciodiret");
+    mysqli_set_charset($conn,"utf8");
+
+    // Test whether the connection is working
+    if (!$conn) {
+        // echo "<strong class='text-danger'>Error:</strong> Failed to connect to the MySQL database." . PHP_EOL;
+        // echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+        // echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+        exit;
+    }
+
+    // echo "<strong class='text-success'>Success:</strong> Success when connecting to the MySQL database." . PHP_EOL;
